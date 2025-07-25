1cb131cb1f4f9c6db204590e9b0c14296c501c02
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-07-26 02:46:28 +0400

    creates function

--- src/main/resources/db/changelog/functions/get_range_by_id.sql
+++ src/main/resources/db/changelog/functions/get_range_by_id.sql
@@ -1,14 +1,14 @@
 --liquibase formatted sql
 --changeset sergei:ee0ca42d-9486-4383-bd1d-7a2ff8f493b0
 
-CREATE TABLE IF NOT EXISTS users_table (
-                                           id    BIGINT PRIMARY KEY,
-    given_name    VARCHAR(25) NOT NULL,
-    family_name    VARCHAR(25) NOT NULL,
-    email  VARCHAR(35) NOT NULL,
-    password    VARCHAR(64) NOT NULL,
-    verbs TEXT
+create or replace function get_range_by_id (p_id text) returns text as
+$$
+declare
+    res text;
+begin
+    select into res range from aircrafts_data where aircraft_code = p_id;
+    return res;
+end
+$$ language 'plpgsql';
 
-    );
-
---rollback DROP TABLE user;
+--rollback drop function get_range_by_id (p_id text);
