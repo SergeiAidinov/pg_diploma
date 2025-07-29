81858189df1134b4571132f7f4c92527f325a67f
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-07-29 04:28:04 +0400

    optimized func. quantity_of_bookings_by_quantity_of_passengers_in_one_booking

--- src/main/resources/db/changelog/functions/quantity_of_bookings_by_quantity_of_passengers_in_one_booking.sql
+++ src/main/resources/db/changelog/functions/quantity_of_bookings_by_quantity_of_passengers_in_one_booking.sql
@@ -8,14 +8,7 @@ create or replace function quantity_of_bookings_by_quantity_of_passengers_in_one
 as
 $$
 begin
-    return query select passengers_in_booking, bookings_totaly
-                 from (select count(book_ref) as bookings_totaly, passengers_in_booking
-                       from (select book_ref, count(ticket_no) as passengers_in_booking
-                             from tickets
-                             where book_ref in (select book_ref from bookings as q)
-                             group by tickets.book_ref) as w
-                       group by passengers_in_booking)
-                          as e
-                 order by passengers_in_booking;
+    return query select ticket_qty, count(br) from (select book_ref as br, count(ticket_no) as ticket_qty from tickets group by book_ref) as q
+                 group by ticket_qty order by ticket_qty;
 end;
 $$;
