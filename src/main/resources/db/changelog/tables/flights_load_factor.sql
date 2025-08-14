aba3ecc8dc09ecf650efd40b3ee8832bc649f10b
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-08-14 04:41:17 +0400

    creates table flights_load_factor

---
+++ src/main/resources/db/changelog/tables/flights_load_factor.sql
@@ -0,0 +1,7 @@
+
+
+create table flights_load_factor (
+                                     flight_id bigint,
+                                     load_factor decimal not null ,
+                                     constraint flight_id_fk foreign key (flight_id) references flights (flight_id)
+);
