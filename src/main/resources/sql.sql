a774702c4eda7c39f7dc705465cfb12ece06d7c0
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-08-22 02:32:49 +0400

    made create_booking

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -1,8 +1,8 @@
-/*select create_booking(2880, 'EDUTTR',
+select create_booking(2880, 'EDUTTR',
                       Array [
                           row('Sergei Aidinov', '7002 842823', 12000, '{"phone": "+70131833325"}')::passenger_and_ticket_price_type
                           ]
-       );*/
+       );
 
 CREATE TABLE orders (
                         ticket_no      char(13)    not null
